3d4336733b0ddbef0c7c453938e4d261ab9f8881
Aaveg Gupta <55689811+aaveggupta@users.noreply.github.com>
2020-10-30 19:28:43 +0530

    Create 2. BS on Reverse Sorted Array.cpp

---
+++ Binary Search/2. BS on Reverse Sorted Array.cpp
@@ -0,0 +1,56 @@
+/*********************
+/Author -> AAVEG GUPTA
+*********************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+#define int ll
+
+
+// BINARY SEARCH ON REVERSE SORTED ARRAY
+// Time Complexity -> O(log(n)) 
+int binarySearch(vector <int> v, int element) {
+	int start = 0, end = v.size() - 1;
+	
+	while (start <= end) {
+
+		int mid = start + ((end - start) / 2);
+		
+		if (v[mid] == element) {
+			return mid;
+		} else if (v[mid] > element) {
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+	}
+	
+	return -1;
+	
+}
+
+
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   
+   int n;
+   cin >> n;
+   
+   vector <int> v(n);
+   for (int i = 0; i < n; i ++) {
+   	cin >> v[i];
+   }
+   
+   int element;
+   cin >> element;
+   
+   int ind = binarySearch(v, element);
+   
+   cout << ind;
+ 
+   return 0;
+}

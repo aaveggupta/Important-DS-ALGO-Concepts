10bd3d302009adb68826f73794a5962510f9b199
Aaveg Gupta <55689811+aaveggupta@users.noreply.github.com>
2020-10-30 19:43:44 +0530

    Create BS on Order Agnostic.cpp

---
+++ Binary Search/BS on Order Agnostic.cpp
@@ -0,0 +1,81 @@
+/*********************
+/Author -> AAVEG GUPTA
+*********************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+#define int ll
+
+
+// BINARY SEARCH ON ORDER AGNOSTIC SEARCH
+// In this case, we don't know whether array is in asc or desc order
+
+int binarySearchDesc(vector <int> v, int element) {
+	int start = 0, end = v.size() - 1;
+	
+	while (start <= end) {
+
+		int mid = start + ((end - start) / 2);
+		
+		if (v[mid] == element) {
+			return mid;
+		} else if (v[mid] > element) {
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+	}
+	
+	return -1;
+}
+
+int binarySearchAsc(vector <int> v, int element) {
+	int start = 0, end = v.size() - 1;
+	
+	while (start <= end) {
+
+		int mid = start + ((end - start) / 2);
+		
+		if (v[mid] == element) {
+			return mid;
+		} else if (v[mid] > element) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+	}
+	
+	return -1;
+}
+
+
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   
+   int n;
+   cin >> n;
+   
+   vector <int> v(n);
+   for (int i = 0; i < n; i ++) {
+   	cin >> v[i];
+   }
+   
+   int element;
+   cin >> element;
+   
+   int ind;
+   
+   if (v[0] < v[1]) {
+   	ind = binarySearchAsc(v, element);
+   } else {
+   	ind = binarySearchDesc(v, element);
+   }
+   
+   cout << ind;
+ 
+   return 0;
+}
